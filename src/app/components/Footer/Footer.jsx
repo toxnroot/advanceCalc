830f89fc44e19bae553f3936c16890a11903d6fe
toxnroot <mohammedking.655@gmail.com>
2025-02-14 07:51:51 +0200

    add footer

---
+++ src/app/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css';
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p className='text'>Created by <a href="http://mohammed-kamal.netlify.app/">Mohammed Kamal</a></p>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Container  from "./components/container/Container";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
     <div >
       <Container/>
+      <Footer/>
     </div>
   );
 }
